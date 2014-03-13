dfedfb0d68aaf9e67683ff6783c768f250001029
jaakaappi <diapaasi@gmail.com>
2014-03-13 10:28:50 +0200

    Fixing the chat and separating the game rooms from the lobby

--- OpenCiv/Assets/Scripts/Manager.cs
+++ OpenCiv/Assets/Scripts/Manager.cs
@@ -2,23 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 
-/// <summary>
-/// Handles nearly everything, core of everything
-/// </summary>
-
 public class Manager : Photon.MonoBehaviour
 {
     public float version = 1.0f;
     public string guistate = "connecting";
+    public bool menu = false;
     public float presetResolution_width = 1280, presetResolution_height = 720;
+    public GUISkin skin;
 
     private float reconnectDelay = 0f, reconnectTime = 0f;
     private string chatMessage = "";
-    public List<string> chatMessages = new List<string> { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+    List<string> chatMessages = new List<string>(11);
 
     void Start()
     {
         StartCoroutine("Connect");
+        Debug.Log(chatMessages.Capacity);
     }
 
     void Update()
@@ -28,6 +27,7 @@ public class Manager : Photon.MonoBehaviour
 
     void OnGUI()
     {
+        GUI.skin = skin;
         Vector3 scale = new Vector3((float)Screen.width / presetResolution_width, (float)Screen.height / presetResolution_height, 1);
         GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
 
@@ -43,39 +43,85 @@ public class Manager : Photon.MonoBehaviour
                 GUILayout.BeginArea(new Rect(0, 0, 1280, 720));
                 GUILayout.BeginHorizontal();
                 GUILayout.Box("Games", GUILayout.Width(900), GUILayout.Height(720));
+                GUILayout.BeginArea(new Rect(10, 10, 900, 720));
+                if (PhotonNetwork.GetRoomList().Length > 0)
+                {
+                    foreach (RoomInfo room in PhotonNetwork.GetRoomList())
+                    {
+                        GUILayout.Label(room.name + " " + room.playerCount + "/" + room.maxPlayers);
+                    }
+                }
+                else
+                    GUILayout.Label("There are no games available.");
+                GUILayout.EndArea();
                 GUILayout.BeginVertical();
-                GUILayout.Box("", GUILayout.Width(380), GUILayout.Height(300));
-                GUILayout.Box("", GUILayout.Width(380), GUILayout.Height(420));
-                GUILayout.BeginArea(new Rect(910, 300, 380, 390));
+                GUILayout.Box("", GUILayout.Width(380), GUILayout.Height(720));
+                GUILayout.BeginArea(new Rect(910, 10, 370, 710));
+                GUILayout.BeginVertical();
+                if (GUILayout.Button("New game"))
+                {
+                    PhotonNetwork.CreateRoom(null, true, true, 20);
+                    guistate = "room";
+                }
+                if (GUILayout.Button("Join game"))
+                {
+
+                }
+                if (PhotonNetwork.GetRoomList().Length > 0)
+                {
+                    if (GUILayout.Button("Join random room"))
+                    {
+                        PhotonNetwork.JoinRandomRoom();
+                        guistate = "room";
+                    }
+                }
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+                GUILayout.EndVertical();
+                GUILayout.EndArea();
+                GUILayout.EndHorizontal();
+                break;
+            case "room":
+                GUILayout.BeginArea(new Rect(0, 0, 1280, 720));
+                GUILayout.BeginHorizontal();
+                GUILayout.Box("Players", GUILayout.Width(900), GUILayout.Height(720));
+                GUILayout.BeginVertical();
+                GUILayout.Box("", GUILayout.Width(380), GUILayout.Height(320));
+                GUILayout.Box("", GUILayout.Width(380), GUILayout.Height(360));
+                GUILayout.EndVertical();
+                GUILayout.BeginArea(new Rect(910, 330, 380, 360));
                 GUILayout.BeginVertical();
                 for (int i = 0; i < chatMessages.Count; i++)
                 {
-                    GUI.Label(new Rect(0, 390- i * 22-22, 380, 22), chatMessages[i]);
+                    GUI.Label(new Rect(0, 340 - i * skin.customStyles[0].fontSize - skin.customStyles[0].fontSize, 380, skin.customStyles[0].fontSize), chatMessages[i], skin.customStyles[0]);
                 }
                 GUILayout.EndVertical();
                 GUILayout.EndArea();
-                GUILayout.EndVertical();
-                GUILayout.EndHorizontal();
-                GUILayout.EndArea();
-
-                GUI.SetNextControlName("asd");
                 chatMessage = GUI.TextArea(new Rect(900, 690, 380, 30), chatMessage);
                 if (chatMessage.Contains("\n"))
                 {
-                    if (chatMessage.StartsWith("/name"))
+                    if (chatMessage.Length > 1)
                     {
-                        chatMessage = chatMessage.Replace("/name", "");
-                        chatMessage = chatMessage.Trim();
-                        PhotonNetwork.playerName = chatMessage;
-                        chatMessage = "";
+                        if (chatMessage.StartsWith("/name"))
+                        {
+                            chatMessage = chatMessage.Replace("/name", "");
+                            chatMessage = chatMessage.Trim();
+                            PhotonNetwork.playerName = chatMessage;
+                            chatMessage = "";
+                        }
+                        else
+                        {
+                            chatMessage.Trim();
+                            photonView.RPC("Message", PhotonTargets.All, chatMessage);
+                            chatMessage = "";
+                        }
                     }
                     else
                     {
-                        chatMessage.Trim();
-                        photonView.RPC("Message", PhotonTargets.All, chatMessage);
                         chatMessage = "";
                     }
                 }
+                GUILayout.EndArea();
                 break;
         }
     }
@@ -103,32 +149,36 @@ public class Manager : Photon.MonoBehaviour
     void OnJoinedLobby()
     {
         StopCoroutine("Connect");
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    void OnJoinedRoom()
-    {
         guistate = "connected";
-    }
-
-    void OnPhotonRandomJoinFailed()
-    {
-        PhotonNetwork.CreateRoom("room", true, true, 20);
-    }
-
-    void OnPhotonCreateRoomFailed()
-    {
-        PhotonNetwork.JoinRandomRoom();
     }
 
     [RPC]
     void Message(string message, PhotonMessageInfo info)
     {
         message.Trim();
-        for (int i = chatMessages.Count - 1; i > 0; i--)
+        if (chatMessages.Count > 0)
         {
-            chatMessages[i] = chatMessages[i - 1];
+            if (chatMessages.Capacity >= chatMessages.Count)
+            {
+                for (int i = chatMessages.Count - 1; i > 0; i--)
+                {
+                    chatMessages[i] = chatMessages[i - 1];
+                }
+                chatMessages.Insert(0,info.sender.name + ": " + message);
+            }
+            else
+            {
+                for (int i = chatMessages.Count - 1; i > 0; i--)
+                {
+                    chatMessages[i] = chatMessages[i - 1];
+                }
+                chatMessages.RemoveAt(0);
+                chatMessages.Insert(0, info.sender.name + ": " + message);
+            }
         }
-        chatMessages[0] = info.sender.name + ": " + message;
+        else
+        {
+            chatMessages.Add(info.sender.name + ": " + message);
+        }
     }
 }
